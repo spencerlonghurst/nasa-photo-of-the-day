b82e428b7c275d1202a570a158a587653912dfab
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-02-17 14:31:29 -0700

    Added some styling; git commit
    ls
    
    cd

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+<App />
+</ThemeProvider>
+,
+ document.getElementById("root")
+);
